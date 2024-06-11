b0d6d205f39decf90a3ee13bfac334b745fb5d00
piyushparmar911 <prparmar911@gmail.com>
2024-06-11 20:19:19 +0530

    car module updated

--- carfix/car/admin.py
+++ carfix/car/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Car
 # Register your models here.
 
-admin.site.register(Car)
+    
+
+class Caradmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'image')
+    
+admin.site.register(Car,Caradmin)
+    
--- carfix/car/models.py
+++ carfix/car/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Car(models.Model):
     firstname = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/', null=True)
